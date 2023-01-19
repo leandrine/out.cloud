64b363f4bd02e07eb07b58b5c229f3f4268af56d
anderson.leandrine <anderson.leandrine@gmail.com>
2023-01-19 15:12:36 -0300

    third second first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,6 +16,16 @@ const config = {
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
+scripts: [
+    
+    {
+      src:
+        '/js/instana.js',
+      async: false,
+    },
+        'defer crossorigin="anonymous" src="https://eum.instana.io/eum.min.js"',
+  ],
+
   presets: [
     [
       'classic',
